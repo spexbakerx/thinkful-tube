a5103900ad31403a981000bb0f3f40ab5b506e22
Lauren Baker <spexbakerx@gmail.com>
2018-02-18 18:03:22 -0800

    image api working

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
-var GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
+var YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 
 var RESULT_HTML_TEMPLATE = (
   '<div>' +
     '<h2>' +
-    '<a class="js-result-name" href="" target="_blank"></a> by <a class="js-user-name" href="" target="_blank"></a></h2>' +
-    '<p>Number of watchers: <span class="js-watchers-count"></span></p>' + 
-    '<p>Number of open issues: <span class="js-issues-count"></span></p>' +
+    '<a class="js-video-name" href="" target="_blank"></a></h2>' +
+    // '<a class="js-video-thumbnail" src="" id="imageID">' +
+    '<img id="imgDisp" src="" />' + 
   '</div>'
 );
 
 function getDataFromApi(searchTerm, callback) {
-  var settings = {
-    url: GITHUB_SEARCH_URL,
-    data: {
-      q: searchTerm + " in:name",
-      per_page: 5
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback
-  };
-  $.ajax(settings);
+    var params = {
+      url: YOUTUBE_SEARCH_URL,
+      data :{
+        part: 'snippet',
+        key: 'AIzaSyDt72bD7KAavoflkuTJMvzma_qSOuk6qIA',
+        q: searchTerm,
+        maxResults: '5'
+      },
+
+      dataType: 'json',
+      type: 'GET',
+      success: callback
+    };
+  
+    $.ajax(params);
 }
 
 
+
+// function renderResult(result) {
+//   var template = $(RESULT_HTML_TEMPLATE);
+//   var title = value.snippet.title;
+//   var thumbnail = value.snippet.thumbnails.default.url;
+//   console.log(title);
+//   template.find(".js-video-name").text(title).attr("href", result.html_url);
+//   template.find(".js-video-thumbnail").thumbnail.attr("href", result.owner.html_url);
+//   // template.find(".js-issues-count").text(result.open_issues);
+//   return template;
+// }
+
 function renderResult(result) {
+  console.log(result);
   var template = $(RESULT_HTML_TEMPLATE);
-  template.find(".js-result-name").text(result.name).attr("href", result.html_url);
-  template.find(".js-user-name").text(result.owner.login).attr("href", result.owner.html_url);
-  template.find(".js-watchers-count").text(result.watchers_count);
-  template.find(".js-issues-count").text(result.open_issues);
+  var imageURL = result.snippet.thumbnails.medium.url;
+  console.log(imageURL);
+  template.find(".js-video-name").text(result.snippet.title).attr("href", result.etag);
+  // template.find(".js-video-thumbnail").text(result.snippet.thumbnails.medium.url);
+  template.find("#imgDisp").attr("src", imageURL);
+
   return template;
 }
 
-function displayGitHubSearchData(data) {
+function displayYouTubeSearchData(data) {
   var results = data.items.map(function(item, index) {
     return renderResult(item);
   });
@@ -48,7 +67,7 @@ function watchSubmit() {
     var query = queryTarget.val();
     // clear out the input
     queryTarget.val("");
-    getDataFromApi(query, displayGitHubSearchData);
+    getDataFromApi(query, displayYouTubeSearchData);
   });
 }
 
